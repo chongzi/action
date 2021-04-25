c498c0d3815d65b7f8512eb540afccb9e2d96624
Xinx1201 <z975203723@gmail.com>
2021-04-25 13:31:58 +0800

    - 修改 模板内容 - 添加 步步多无限刷金币[自定义次数 5000]

--- Task/bubuduo.js
+++ Task/bubuduo.js
@@ -1,35 +1,68 @@
+/*
+ * @Author: Xin https://github.com/Xin-code 
+ * @Date: 2021-04-25 12:43:05 
+ * @Last Modified by: Xin 
+ * @Last Modified time: 2021-04-25 13:31:20
+ */
 
-const $ = Env('xxx')
+const $ = Env('步步多')
 
-const notify = $.isNode() ? require('./sendNotify') : '';
+const BBD_API_HOST = 'https://bp-api.coohua.com'
 
-$.message = ''
+const accessKeyArr = []
 
-const XXXX_API_HOST = ''
+const deviceIdArr = []
 
-// if ($.isNode()) {
-  // if (process.env.XXXX_XXXXX && process.env.XXXX_XXXXX.indexOf('#') > -1) {
-  //   xxxx = process.env.XXXX_XXXXX.split('#');
-  // }else if(process.env.XXXX_XXXXX && process.env.XXXX_XXXXX.indexOf('#') > -1) {
-  //   xxxx = process.env.XXXX_XXXXX.split('\n');
-  // }else{
-  //   xxxx = [process.env.XXXX_XXXXX]
-  // }
+// 自定义刷的次数 
+$.rushtime = 5000
 
-  // Object.keys(xxxx).forEach((item) => {
-  //   if (xxxx[item]) {
-  //     XXXXxxxx.push(xxxx[item])
-  //   }
-  // })
-// }
+// 总共获得金币数量
+$.total = 0
+
+
+if ($.isNode()) {
+  if (process.env.BBD_ACCESS_KEY && process.env.BBD_ACCESS_KEY.indexOf('#') > -1) {
+    accessKey = process.env.BBD_ACCESS_KEY.split('#');
+  }else if(process.env.BBD_ACCESS_KEY && process.env.BBD_ACCESS_KEY.indexOf('#') > -1) {
+    accessKey = process.env.BBD_ACCESS_KEY.split('\n');
+  }else{
+    accessKey = [process.env.BBD_ACCESS_KEY]
+  }
+
+  Object.keys(accessKey).forEach((item) => {
+    if (accessKey[item]) {
+      accessKeyArr.push(accessKey[item])
+    }
+  })
+
+  if (process.env.BBD_DEVICE_ID && process.env.BBD_DEVICE_ID.indexOf('#') > -1) {
+    deviceId = process.env.BBD_DEVICE_ID.split('#');
+  }else if(process.env.BBD_DEVICE_ID && process.env.BBD_DEVICE_ID.indexOf('#') > -1) {
+    deviceId = process.env.BBD_DEVICE_ID.split('\n');
+  }else{
+    deviceId = [process.env.BBD_DEVICE_ID]
+  }
+
+  Object.keys(deviceId).forEach((item) => {
+    if (deviceId[item]) {
+      deviceIdArr.push(deviceId[item])
+    }
+  })
+}
 
 !(async () => {
-  for (let i = 0; i < XXXX.length; i++) {
+  for (let i = 0; i < accessKeyArr.length; i++) {
+    accesskey = accessKeyArr[i]
+    deviceid = deviceIdArr[i]
 
     console.log(`········【帐号${i+1}】开始········`)
 
-    // 任务
-    await xxx()
+    // 开始刷金币
+    console.log(`💰执行 -> 开始刷金币`)
+    for(let i = 0 ; i < $.rushtime ; i++){
+      await Rush()
+    }
+    console.log(`本次一共获得金币:${$.total}个💰金币`);
     
     // 推送消息
     // await sendMsg()
@@ -41,16 +74,19 @@ const XXXX_API_HOST = ''
     .catch((e) => $.logErr(e))
     .finally(() => $.done())
     
-// 任务
-async function xxx() {
+// 刷金币💰
+async function Rush() {
   // 调用API
-  await xxx_API()
-  let result = JSON.parse($.xxx_API_Result)
-  if(result.code!==1){
-    console.log(`❌ ${result.msg}`)
+  await Rush_API()
+
+  if(result.code!==0){
+    console.log(`❌ 刷金币错误`)
   }else{
-    console.log(`获得···`)
+    // console.log(result)
+    console.log(`刷金币成功,本次获得金币:${result.result}个金币💰`)
+    $.total+=result.result
   }
+  
 }
 
 // 推送消息
@@ -60,26 +96,18 @@ async function sendMsg() {
 
 // ==================API==================
 // API
-async function xxx_API() {
-  await postRequest(``)
+async function Rush_API() {
+  await getRequest(`bp/ad/award/reportV2?accessKey=${accesskey}&amount=10&anomy=0&appId=2&appVersion=1.0.9&brand=Apple&bs=CDMA&channel=App%20Store&deviceId=${deviceid}&gps=30%2C120&isPass=1&os=iOS&osVersion=iOS%2014&pkgId=3&posName=double&romVersion=iOS%2014&sign=15f16394fc8573945f06cb78be626ee2&version=1.0.9`)
 }
-
-// API
-async function xxx_XXX_API() {
-  let body = ``
-  $.xxx_API_Result = await postRequestBody(``,body)
-}
-
-
 
 // ==================请求==================
 
 // 正常请求 增加代码的复用率
 // RequestBody
-function postRequest(function_id, timeout = 1000){
+function getRequest(function_id, timeout = 1000){
   return new Promise(resolve => {
     setTimeout(() => {
-      $.post(taskUrl(function_id), (err, resp, data) => {
+      $.get(taskUrl(function_id), (err, resp, data) => {
         try {
           if (err) {
             console.log('\nAPI查询请求失败 ‼️‼️')
@@ -101,48 +129,22 @@ function postRequest(function_id, timeout = 1000){
 // URL
 function taskUrl(activity) {
   return {
-    url: `${XXXX_API_HOST}/${activity}`,
+    url: `${BBD_API_HOST}/${activity}`,
     headers: {
-      'Host': '',
-      'User-Agent': '',
+      'Host': 'bp-api.coohua.com',
+      'accessKey':accesskey,
+      'deviceId': deviceid,
+      'appVersion': '1.0.9',
+      'brand': 'Apple',
+      'channel': 'App Store',
+      'gps': '30,120',
+      'os': 'iOS',
+      'osVersion': 'iOS 14',
+      'romVersion': 'iOS 14',
     }
   }
 }
 
-// 带Body的请求 增加代码的复用率
-// RequestBody
-function postRequestBody(function_id, body = {}, timeout = 1000){
-  return new Promise(resolve => {
-    setTimeout(() => {
-      $.post(BodytaskUrl(function_id, body), (err, resp, data) => {
-        try {
-          if (err) {
-            console.log('\nAPI查询请求失败 ‼️‼️')
-            console.log(JSON.stringify(err));
-            console.log(`function_id:${function_id}`)
-          } else {
-            result = JSON.parse(data);
-          }} catch (e) {
-            console.log(e)
-        } finally {
-          resolve(data);
-        }
-      })
-    }, timeout)
-  })
-} 
-
- // BODYURL
- function BodytaskUrl(activity, body={}) {
-  return {
-    url: `${XXXX_API_HOST}/${activity}`,
-    body: body,
-    headers: {
-      'Host': '',
-      'User-Agent': '',
-    }
-  }
-}
 
 // pretty-ignore
 function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),a={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(a,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon()?(this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)})):this.isQuanX()?(this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t))):this.isNode()&&(this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();s&&this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)}))}post(t,e=(()=>{})){if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.post(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method="POST",this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){this.initGotEnv(t);const{url:s,...i}=t;this.got.post(s,i).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>{const{message:s,response:i}=t;e(s,i,i&&i.body)})}}time(t){let e={"M+":(new Date).getMonth()+1,"d+":(new Date).getDate(),"H+":(new Date).getHours(),"m+":(new Date).getMinutes(),"s+":(new Date).getSeconds(),"q+":Math.floor(((new Date).getMonth()+3)/3),S:(new Date).getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,((new Date).getFullYear()+"").substr(4-RegExp.$1.length)));for(let s in e)new RegExp("("+s+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?e[s]:("00"+e[s]).substr((""+e[s]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl;return{"open-url":e,"media-url":s}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};if(this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r))),!this.isMuteLog){let t=["","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];t.push(e),s&&t.push(s),i&&t.push(i),console.log(t.join("\n")),this.logs=this.logs.concat(t)}}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
